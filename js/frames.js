b8944e445eaa6cfc33a4fc4a8088769d4e04f0b2
encuentros-gianuc2 <encuentrosgianuc2@gmail.com>
2025-09-25 09:23:41 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -12,7 +12,7 @@ function frames(variable){
     } 
     else if (variable==3){
         document.getElementById("enframe").src="pages/postulacion.html";
-        document.getElementById("enframe").height="1050";
+        document.getElementById("enframe").height="1200";
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/patrocinadores.html";
